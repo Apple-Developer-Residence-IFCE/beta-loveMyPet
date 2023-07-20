3d4062cc529514315f7cb2934a113b4a16f8bcdb
Rn246 <raynery246@gmail.com>
2023-07-20 19:44:57 -0300

    feature 013

--- LoveMyPet/App/Utils/007-Picker Row.swift
+++ LoveMyPet/App/Utils/007-Picker Row.swift
@@ -1,73 +1,73 @@
+////
+////  ContentView.swift
+////  Interface
 //
-//  ContentView.swift
-//  Interface
-
-//  Created by userext on 04/07/23.
+////  Created by userext on 04/07/23.
+////
 //
-
-import SwiftUI
-
-struct Options: Hashable {
-    let name: String
-}
-
-let genderOptions = [
-    Options(name: "Masculino"),
-    Options(name: "Feminino"),
-    Options(name: "Outro"),
-    Options(name: "Nenhum"),
-    Options(name: "Não escolhida")
-]
-
-let speciesOptions = [
-    Options(name: "Cachorro"),
-    Options(name: "Gato"),
-    Options(name: "Pássaro"),
-    Options(name: "Nenhum"),
-    Options(name: "Não escolhida")
-]
-
-let raceOptions = [
-    Options(name: "Labrador"),
-    Options(name: "Persa"),
-    Options(name: "Canário"),
-    Options(name: "Nenhum"),
-    Options(name: "Não escolhida")
-]
-
-struct PickerView: View {
-    @State private var selectedGender = genderOptions[0]
-    @State private var selectedSpecies = speciesOptions[0]
-    @State private var selectedRace = raceOptions[0]
-
-    var body: some View {
-        VStack {
-            List {
-                Picker(selection: $selectedGender, label: Text("Gênero")) {
-                    ForEach(genderOptions, id: \.name) { option in
-                        Text(option.name).tag(option)
-                    }
-                }
-                .pickerStyle(.automatic)
-                Picker(selection: $selectedSpecies, label: Text("Espécies")) {
-                    ForEach(speciesOptions, id: \.name) { option in
-                        Text(option.name).tag(option)
-                    }
-                }
-
-                .pickerStyle(.automatic)
-                Picker(selection: $selectedRace, label: Text("Raça")) {
-                    ForEach(raceOptions, id: \.name) { option in
-                        Text(option.name).tag(option)
-                    }
-                }
-                .pickerStyle(.automatic)
-            }
-        }}
-}
-
-struct Picker_Previews: PreviewProvider {
-    static var previews: some View {
-        PickerView()
-    }
-}
+//import SwiftUI
+//
+//struct Options: Hashable {
+//    let name: String
+//}
+//
+//let genderOptions = [
+//    Options(name: "Masculino"),
+//    Options(name: "Feminino"),
+//    Options(name: "Outro"),
+//    Options(name: "Nenhum"),
+//    Options(name: "Não escolhida")
+//]
+//
+//let speciesOptions = [
+//    Options(name: "Cachorro"),
+//    Options(name: "Gato"),
+//    Options(name: "Pássaro"),
+//    Options(name: "Nenhum"),
+//    Options(name: "Não escolhida")
+//]
+//
+//let raceOptions = [
+//    Options(name: "Labrador"),
+//    Options(name: "Persa"),
+//    Options(name: "Canário"),
+//    Options(name: "Nenhum"),
+//    Options(name: "Não escolhida")
+//]
+//
+//struct PickerView: View {
+//    @State private var selectedGender = genderOptions[0]
+//    @State private var selectedSpecies = speciesOptions[0]
+//    @State private var selectedRace = raceOptions[0]
+//
+//    var body: some View {
+//        VStack {
+//            List {
+//                Picker(selection: $selectedGender, label: Text("Gênero")) {
+//                    ForEach(genderOptions, id: \.name) { option in
+//                        Text(option.name).tag(option)
+//                    }
+//                }
+//                .pickerStyle(.automatic)
+//                Picker(selection: $selectedSpecies, label: Text("Espécies")) {
+//                    ForEach(speciesOptions, id: \.name) { option in
+//                        Text(option.name).tag(option)
+//                    }
+//                }
+//
+//                .pickerStyle(.automatic)
+//                Picker(selection: $selectedRace, label: Text("Raça")) {
+//                    ForEach(raceOptions, id: \.name) { option in
+//                        Text(option.name).tag(option)
+//                    }
+//                }
+//                .pickerStyle(.automatic)
+//            }
+//        }}
+//}
+//
+//struct Picker_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PickerView()
+//    }
+//}
--- LoveMyPet/Features/ContentView.swift
+++ LoveMyPet/Features/ContentView.swift
@@ -1,20 +1,20 @@
+////
+////  ContentView.swift
+////  LoveMyPet
+////
+////  Created by userext on 17/07/23.
+////
 //
-//  ContentView.swift
-//  LoveMyPet
+//import SwiftUI
 //
-//  Created by userext on 17/07/23.
+//struct ContentView: View {
+//    var body: some View {
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//    }
+//}
 //
-
-import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
